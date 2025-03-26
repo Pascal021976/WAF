0b0b4db6bbc9437a0d4b25f1011f1e6ea0a2ba59
Pascal021976 <psilly005@gmail.com>
2025-03-26 22:03:26 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4f46e5',
+        secondary: '#f43f5e',
+        accent: '#10b981'
+      }
+    }
+  },
+  plugins: []
+};
 
